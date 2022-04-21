67d4095e8e6704a525c5f8d7fca1dfbfc71a66f4
Bit0r <nie_wang@outlook.com>
2022-04-21 13:38:12 +0800

    📦 购物车页面和接口更新

--- controller/cart.go
+++ controller/cart.go
@@ -11,18 +11,49 @@ import (
 )
 
 func setupCart() {
-	cartGroup := router.Group("/cart", auth)
-	cartGroup.GET("/add/:id", handleAddCart)
+	cartGroup := router.Group("/cart")
+	cartGroup.POST("/book", auth, handleAddCart)
+	cartGroup.GET("/books", authRedirect, handleShowCart)
+	cartGroup.PUT("/book", auth, handleUpdateCart)
+}
+
+func handleShowCart(ctx *gin.Context) {
+	id, _ := sessions.Default(ctx).Get("userID").(uint64)
+
+	files := []string{"layout.html", "navbar.html", "shopping-cart.html"}
+
+	data := struct {
+		model.CartBooks
+		model.CartAddresses
+	}{
+		model.GetCartItems(id),
+		model.GetCartAddresses(id)}
+
+	ctx.Set("tpl_files", files)
+	ctx.Set("tpl_data", data)
 }
 
 func handleAddCart(ctx *gin.Context) {
 	session := sessions.Default(ctx)
 	id, _ := session.Get("userID").(uint64)
 
-	bookID, _ := strconv.ParseUint(ctx.Param("id"), 0, 64)
+	bookID, _ := strconv.ParseUint(ctx.PostForm("id"), 0, 64)
 	err := model.AddCartItem(id, bookID)
 	if err != nil {
 		log.Fatal(err)
+		ctx.Status(http.StatusInternalServerError)
+	}
+}
+
+func handleUpdateCart(ctx *gin.Context) {
+	userID := sessions.Default(ctx).Get("userID").(uint64)
+	bookInfo := struct {
+		BookID   uint64
+		Quantity uint
+	}{}
+	ctx.Bind(&bookInfo)
+	err := model.UpdateCartItem(userID, bookInfo.BookID, bookInfo.Quantity)
+	if err != nil {
 		ctx.Status(http.StatusInternalServerError)
 	}
 }
